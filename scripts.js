cbed15d6b645f86d6f05fd4bed0a1f91580bf4b0
ajohaa <527ahannah@frhsd.com>
2026-02-04 09:31:55 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,78 @@
+function getUserName() {
+    const userName = document.getElementById('userName').value;
+    return userName;
+}
+
+function welcomeMessage() {
+    const userName = getUserName();
+    const message = `Welcome, ${userName}, to Rock-Paper-Scissors!`;
+    document.getElementById('welcomeMessage').innerText = message;
+}
+welcomeMessage();
+
+const choices = ["rock", "paper", "scissors"];
+
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    return userInput;
+  } else {
+    console.log("Error!");
+  }
+};
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return "The game is a tie!";
+  }
+  if (userChoice === "rock") {
+    if (computerChoice === "paper") {
+      return "Computer won.";
+    } else {
+      return "You won!";
+    }
+  }
+
+  if (userChoice === "paper") {
+    if (computerChoice === "scissors") {
+      return "Computer won.";
+    } else {
+      return "You won!";
+    }
+  }
+
+  if (userChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "Computer won.";
+    } else {
+      return "You won!";
+    }
+  }
+};
+
+const playGame = () => {
+  const userChoice = getUserChoice("scissors");
+  const computerChoice = getComputerChoice();
+  console.log(`You chose: ${userChoice}.`);
+  console.log(`Computer chose: ${computerChoice}.`);
+
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
